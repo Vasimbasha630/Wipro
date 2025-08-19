3daa4d54483abb31ff83146c309d92f582a8608e
Basha <owk323011@gmail.com>
2025-08-19 16:53:45 +0530

    Message

---
+++ MvcAdoEmployCrud/MvcAdoEmployCrud/Models/Employ.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcAdoEmployCrud.Models
+{
+    public class Employ
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // 👈 Auto-generate Empno
+        [Display(Name = "Employ Number")]
+        public int Empno { get; set; }
+
+        [Display(Name = "Employ Name")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Gender")]
+        public string? Gender { get; set; }
+
+        [Display(Name = "Department")]
+        public string? Dept { get; set; }
+
+        [Display(Name = "Designation")]
+        public string? Desig { get; set; }
+
+        [Display(Name = "Salary")]
+        [Column("Basic")]
+        public decimal Basic { get; set; }
+    }
+}
